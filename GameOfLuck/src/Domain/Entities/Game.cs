30e4e28d6fd0558d3eaa5c1177718f06ec9b6beb
shahzaibzafar <shahzaibzafar@hotmail.com>
2023-12-09 03:29:11 +0100

    models

---
+++ GameOfLuck/src/Domain/Entities/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLuck.Domain.Entities;
+internal class Game
+{
+    private int randomNumber;
+    private List<Player> players = new List<Player>();
+    private Random random = new Random();
+    public void NewGame()
+    {
+        players.Clear();
+        randomNumber = random.Next(10);
+    }
+    public void AddPlayer(Player player)
+    {
+        players.Add(player);
+    }
+
+    public void RemovePlayer(Player player) { players.Remove(player); }
+
+    
+}
